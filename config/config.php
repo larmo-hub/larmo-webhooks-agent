0ba73b42c98edc9f802aafcb28c66cedcbe2b5aa
Mateusz Ksiazek <ksiazek.mate@gmail.com>
2015-09-09 13:17:15 +0200

    Added signature verification  to scrutinizer service

--- config/config.php
+++ config/config.php
@@ -5,6 +5,7 @@ $config = [
     'hubURI' => getenv('hub_uri'),
     'signatures' => [
         'github' => getenv('github_secret_signature'),
-        'travis' => getenv('travis_secret_signature')
+        'travis' => getenv('travis_secret_signature'),
+        'scrutinizer' => getenv('scrutinizer_secret_signature')
     ]
 ];
